63106af07b9fc2beb745f6df84aeaacd9c648142
cielarchazure <chungwinzun7765@gmail.com>
2025-11-02 00:44:40 -0600

    Added animations in picom. Added tmux and nvim config. Slight adjustment to qtile config.

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,28 +1,28 @@
 import os
 
-import libqtile.resources
 from libqtile import bar, layout, qtile, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.widget import GenPollText
 from libqtile import widget as defWidget
 
-#qtile extra
+# qtile extra
 from qtile_extras.widget.decorations import PowerLineDecoration
-from qtile_extras import widget 
+from qtile_extras import widget
 
-#colors
-from colors import colors 
+# colors
+from colors import colors
 
-#custom layout
+# custom layout
 from dwindle import Dwindle
 
 import subprocess
 
+
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
-    subprocess.Popen(['bash', home])
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.Popen(["bash", home])
+
 
 mod = "mod4"
 terminal = "kitty"
@@ -38,14 +38,23 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -69,22 +78,57 @@ keys = [
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key(
+        [mod],
+        "t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
     Key([mod, "control"], "u", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
     Key([mod], "d", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
-    Key([], "Print", lazy.spawn("sh -c 'maim -s | xclip -selection clipboard -t image/png'"), desc="Capture area to clipboard"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +2%"), desc="Volume up"),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -2%"), desc="Volume down"),
-    Key([mod], "XF86AudioRaiseVolume", lazy.spawn("brightnessctl set 5%+"), desc="Increase brightness"),
-    Key([mod], "XF86AudioLowerVolume", lazy.spawn("brightnessctl set 5%-"), desc="Decrease brightness"),
-
+    Key(
+        [],
+        "Print",
+        lazy.spawn("sh -c 'maim -s | xclip -selection clipboard -t image/png'"),
+        desc="Capture area to clipboard",
+    ),
+    Key(
+        [],
+        "XF86AudioRaiseVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +2%"),
+        desc="Volume up",
+    ),
+    Key(
+        [],
+        "XF86AudioLowerVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -2%"),
+        desc="Volume down",
+    ),
+    Key(
+        [mod],
+        "XF86AudioRaiseVolume",
+        lazy.spawn("brightnessctl set 5%+"),
+        desc="Increase brightness",
+    ),
+    Key(
+        [mod],
+        "XF86AudioLowerVolume",
+        lazy.spawn("brightnessctl set 5%-"),
+        desc="Decrease brightness",
+    ),
     # --- Dunst Shortcuts ---
-    Key(["control", "shift"], "a", lazy.spawn("dunstctl action"), desc="accept request"),
+    Key(
+        ["control", "shift"], "a", lazy.spawn("dunstctl action"), desc="accept request"
+    ),
     Key(["control"], "space", lazy.spawn("dunstctl close"), desc="close notification"),
-    Key(["control"], "grave", lazy.spawn("dunstctl history-pop"), desc="show dunst history"),
+    Key(
+        ["control"],
+        "grave",
+        lazy.spawn("dunstctl history-pop"),
+        desc="show dunst history",
+    ),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -128,16 +172,16 @@ for i in groups:
     )
 
 layoutFormat = {
-    "border_width" : 4,
-    "margin" : 2,
-    "border_focus" : colors["purple"],
-    "border_normal" : colors["black"]
+    "border_width": 4,
+    "margin": 2,
+    "border_focus": colors["purple"],
+    "border_normal": colors["black"],
 }
 
 layouts = [
     Dwindle(**layoutFormat)
     # layout.Columns(**layoutFormat),
-    #layout.Max(),
+    # layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(**layoutFormat),
@@ -164,22 +208,20 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
+
 def get_brightness():
     try:
         # Run the command and capture the output
         command = "brightnessctl -m | awk -F, '{print $4}' | tr -d '%' "
         result = subprocess.run(
-            command, 
-            shell=True, 
-            capture_output=True, 
-            text=True, 
-            check=True
+            command, shell=True, capture_output=True, text=True, check=True
         )
         # The output is the clean percentage number (e.g., "50")
         brightness = result.stdout.strip()
         return f"  {brightness}%"
     except subprocess.CalledProcessError:
-        return "N/A" # Return N/A if command fails
+        return "N/A"  # Return N/A if command fails
+
 
 powerLine = {
     "decorations": [
@@ -189,72 +231,72 @@ powerLine = {
     ]
 }
 
+
 def batteryWidget():
     return [
         widget.GenPollText(
-            background = colors["red"],
-            fmt = "",
+            background=colors["red"],
+            fmt="",
         ),
         widget.UPowerWidget(
-            background = colors["red"],
-            foreground = colors["white"],
-            battery_height = 15,
-            battery_width = 30,
-            text_charging = "(Plugged In)",
-            text_discharging = "(On Battery)",
+            background=colors["red"],
+            foreground=colors["white"],
+            battery_height=15,
+            battery_width=30,
+            text_charging="(Plugged In)",
+            text_discharging="(On Battery)",
         ),
         widget.Battery(
-            background = colors["red"],
-            foreground = colors["white"],
-            format='{percent:2.0%}',
-            padding = 10,
-            **powerLine
-        )
+            background=colors["red"],
+            foreground=colors["white"],
+            format="{percent:2.0%}",
+            padding=10,
+            **powerLine,
+        ),
     ]
+
 
 def clockWidgets():
-    return[
+    return [
         widget.Clock(
-            background = colors["blue"],
-            foreground = colors["white"],
-            padding = 10,
+            background=colors["blue"],
+            foreground=colors["white"],
+            padding=10,
             format="%Y-%m-%d %a",
-            **powerLine
+            **powerLine,
         ),
         widget.Clock(
-            background = colors["purple"],
-            foreground = colors["white"],
-            padding = 10,
+            background=colors["purple"],
+            foreground=colors["white"],
+            padding=10,
             format="%H:%M",
-        )
+        ),
     ]
 
+
 def CPUWidget():
-    return[
+    return [
         widget.TextBox(
-            background = colors["green"],
-            fmt = "",
-            padding = 5,
+            background=colors["green"],
+            fmt="",
+            padding=5,
         ),
         defWidget.CPU(
-            background = colors["green"],
-            foreground = colors["white"],
-            format = ': {load_percent}%', 
-            update_interval = 1.0,
+            background=colors["green"],
+            foreground=colors["white"],
+            format=": {load_percent}%",
+            update_interval=1.0,
         ),
         defWidget.Memory(
-            background = colors["green"],
-            foreground = colors["white"],
-            format = ': {MemUsed:.2f}GB/{MemTotal:.2f}GB',
-            measure_mem = 'G', 
-            update_interval = 1.0,
+            background=colors["green"],
+            foreground=colors["white"],
+            format=": {MemUsed:.2f}GB/{MemTotal:.2f}GB",
+            measure_mem="G",
+            update_interval=1.0,
         ),
-        widget.TextBox(
-            background = colors["green"],
-            fmt = "",
-            **powerLine
-        )
+        widget.TextBox(background=colors["green"], fmt="", **powerLine),
     ]
+
 
 def init_bar():
     widgets_list = [
@@ -266,38 +308,31 @@ def init_bar():
             chords_colors={"launch": (colors["purple"], colors["blue"])},
             name_transform=lambda name: name.upper(),
         ),
-        widget.Systray(
-            padding = 5,
-            icon_size = 25,
-            **powerLine
-        ),
+        widget.Systray(padding=5, icon_size=25, **powerLine),
         *batteryWidget(),
         widget.Volume(
-            background = colors["orange"],
-            foreground = colors["black"],
-            fmt="   {}", 
+            background=colors["orange"],
+            foreground=colors["black"],
+            fmt="   {}",
             channel="Master",
-            padding = 10,
-            **powerLine
+            padding=10,
+            **powerLine,
         ),
         widget.GenPollText(
-            background = colors["yellow"],
-            foreground = colors["black"],
-            name='brightness_display',
-            func=get_brightness, 
+            background=colors["yellow"],
+            foreground=colors["black"],
+            name="brightness_display",
+            func=get_brightness,
             update_interval=0.5,
-            format='{}',
-            padding = 10,
-            **powerLine
+            format="{}",
+            padding=10,
+            **powerLine,
         ),
         *CPUWidget(),
         *clockWidgets(),
     ]
-    return bar.Bar(
-        widgets_list, 
-        30,
-        margin = [5, 8, 3, 8]
-    )
+    return bar.Bar(widgets_list, 30, margin=[5, 8, 3, 8])
+
 
 screens = [
     Screen(
@@ -310,8 +345,15 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
