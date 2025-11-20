55fb944fae14c62cf71225ff8123ca58ab826341
cielarchazure <chungwinzun7765@gmail.com>
2025-11-20 12:49:04 -0700

    feat(fish): added alias for cbonsai and lazygit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
 
     alias gpt4.1 "uvx llm chat -m github_copilot/gpt-4.1 -s \"Input short answer.\""
     alias gPro "uvx llm chat -m github_copilot/gemini-2.5-pro -s \"Input long answer.\""
+    alias bonsai "cbonsai -li"
+    alias lg lazygit
 
 end
