8c3b5862cd47acda82c3917ce7d284c62d61888c
cielarchazure <chungwinzun7765@gmail.com>
2025-11-23 20:59:30 -0700

    feat(fish): added new greeting message

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,12 @@
 if status is-interactive
+    function fish_greeting
+        set_color normal
+        echo "Welcome to a friendly interactive shell~~"
+        set_color yellow
+        echo -n "Note: "
+        set_color normal
+        echo "use 'nvm use lts' to use npm"
+    end
 
     alias gpt4.1 "uvx llm chat -m github_copilot/gpt-4.1 -s \"Input short answer.\""
     alias gPro "uvx llm chat -m github_copilot/gemini-2.5-pro -s \"Input long answer.\""
