26b73f27cacf904d574e886856080a702bf509ac
cielarchazure <chungwinzun7765@gmail.com>
2025-11-25 17:26:19 -0700

    feat(fish): added alias for weather

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,6 +11,7 @@ if status is-interactive
     alias gpt4.1 "uvx llm chat -m github_copilot/gpt-4.1 -s \"Input short answer.\""
     alias gPro "uvx llm chat -m github_copilot/gemini-2.5-pro -s \"Input long answer.\""
     alias bonsai "cbonsai -li"
+    alias weather "curl -s https://weather.gc.ca/rss/city/ab-50_e.xml | grep \"Current Conditions\""
     alias lg lazygit
 
 end
